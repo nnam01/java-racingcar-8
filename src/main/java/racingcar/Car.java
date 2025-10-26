9051c6c6e1d9f5b56e045c4a041c34753c968523
nnam01 <isthenam@naver.com>
2025-10-26 19:58:13 +0900

    feat: 자동차 이동 기능

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,16 +6,30 @@ public class Car {
     private static final int MAX_NAME_LENGTH = 5;
 
     private final String name;
+    private int movingForward;
 
     public Car(String name) {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException();
         }
         this.name = name;
+        this.movingForward = 0;
     }
 
     @Override
     public String toString() {
-        return name;
+        return name + " : " + this.movingForwardToString();
+    }
+
+    public String movingForwardToString() {
+        String moving = "";
+        for (int i = 0; i < movingForward; i++) {
+            moving += "-";
+        }
+        return moving;
+    }
+
+    public void moveForward() {
+        this.movingForward++;
     }
 }
