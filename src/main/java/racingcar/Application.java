cc44924496bcfe472400bdeeeb5304f73e41b57e
nnam01 <isthenam@naver.com>
2025-10-26 20:47:15 +0900

    feat: 입출력 기능

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Console.*;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CarRacing carRacing = new CarRacing();
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = Console.readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int n = Integer.parseInt(Console.readLine());
+
+        List<Car> cars = carRacing.startCarRacing(carNames, n);
+
+        System.out.println("최종 우승자 : " + cars);
+
     }
 }
--- src/main/java/racingcar/CarRacing.java
+++ src/main/java/racingcar/CarRacing.java
@@ -5,6 +5,40 @@ import java.util.List;
 
 public class CarRacing {
 
+    public List<Car> startCarRacing(String carNames, int n) {
+        List<Car> cars = setCars(carNames);
+
+        for (int i = 0; i < n; i++) {
+            runOneCycle(cars);
+            printRacingStatus(cars);
+        }
+
+        return findWinner(cars);
+    }
+
+    public void runOneCycle(List<Car> cars) {
+        for (Car car : cars) {
+            car.tryMoveForward();
+        }
+    }
+
+    public void printRacingStatus(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.toString());
+        }
+        System.out.println();
+    }
+
+    public List<Car> setCars(String carNames) {
+        List<Car> cars = new ArrayList<>();
+
+        String[] carNamesArray = carNames.split(",");
+        for (String s : carNamesArray) {
+            cars.add(new Car(s));
+        }
+        return cars;
+    }
+
     public List<Car> findWinner(List<Car> cars) {
 
         int maxDistance = 0;
